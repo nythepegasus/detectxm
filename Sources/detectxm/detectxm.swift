2d3576682897e612f8162e42eef0ec8e924c8302
ny <mac@nythepegas.us>
2025-07-04 02:22:37 -0400

    fix: properly detect from iOS app on macOS; add documentation

--- Sources/detectxm/detectxm.swift
+++ Sources/detectxm/detectxm.swift
@@ -8,22 +8,29 @@ extension FileManager {
 }
 
 public extension ProcessInfo {
+    /// Whether the current device supports TXM.
     var hasTXM: Bool {
 #if !canImport(Darwin)
         // people have no reason to use this package on non-Apple platforms
         // however we can at least return a value for those that do (for whatever reason)
         false
 #else
-#if os(iOS) || os(tvOS) || os(watchOS) || os(visionOS) // TODO: verify all of these follow iOS
-        guard let file = FileManager.default.filePath(atPath: "/private/preboot", withLength: 96) else { return false }
-#elseif os(macOS)
+#if os(macOS)
         // macOS varies on base paths slightly
         guard let boot = FileManager.default.filePath(atPath: "/System/Volumes/Preboot", withLength: 36),
               let file = FileManager.default.filePath(atPath: "\(boot)/boot", withLength: 96) else { return false }
+#else
+        if #available(iOS 14.0, *) {
+            if isiOSAppOnMac {
+                guard let boot = FileManager.default.filePath(atPath: "/System/Volumes/Preboot", withLength: 36),
+                      let file = FileManager.default.filePath(atPath: "\(boot)/boot", withLength: 96) else { return false }
+                return access("\(file)/usr/standalone/firmware/FUD/Ap,TrustedExecutionMonitor.img4", F_OK) == 0
+            }
+        }
+        guard let file = FileManager.default.filePath(atPath: "/private/preboot", withLength: 96) else { return false }
 #endif
         // if this file exists, we presume TXM is supported by the current hardware
         return access("\(file)/usr/standalone/firmware/FUD/Ap,TrustedExecutionMonitor.img4", F_OK) == 0
 #endif
     }
 }
-
